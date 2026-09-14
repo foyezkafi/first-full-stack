4db4897083fedf4d03fc38397a4cd984b7fb3b09
foyezkafi <foyezkafi@gmail.com>
2026-09-14 12:39:17 +0600

    push 1

---
+++ client/src/App.jsx
@@ -0,0 +1,102 @@
+import { useEffect, useState } from "react"
+import axios from 'axios';
+
+function App() {
+  const [userlist, setUserList] = useState([])
+  let [FormData, setFromData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  })
+  let [error, setError] = useState({
+    username: "",
+    email: "",
+    password: "",
+  })
+
+  let handleForm = (e) => {
+    let { name, value } = e.target
+    setFromData({ ...FormData, [name]: value })
+    setError({})
+  }
+
+  const handleSubmit = async () => {
+    console.log(FormData)
+    const response = await axios.post('http://localhost:8000/registration', {
+    username: FormData.username,
+    email: FormData.email,
+    password: FormData.password,
+});
+
+   console.log(response);
+  }
+
+  useEffect(()=>{
+  async function fetch(){
+  const response = await axios.get('http://localhost:8000/allusers');
+  setUserList (response.data)
+  console.log(response.data) 
+  
+  }
+  fetch()
+  
+  },[])
+
+  return (
+    <div>
+      <div>
+        <input 
+          onChange={handleForm} 
+          name="username" 
+          type="text" 
+          placeholder="Enter Your Name" 
+        />
+      </div>
+
+      <br />
+
+      <div>
+        <input 
+          onChange={handleForm} 
+          name="email" 
+          type="email" 
+          placeholder="Enter Your email" 
+        />
+      </div>
+
+      <br />
+
+      <div>
+        <input 
+          onChange={handleForm} 
+          name="password" 
+          type="password" 
+          placeholder="Enter Your password" 
+        />
+      </div>
+
+      <br />
+
+      <button onClick={handleSubmit}>Submit</button>
+      <h1>
+        <div className="card_wrapper">
+          {userlist.map((item,index)=>(
+            <div key={index}  className="card_item">
+            <h3>Name: {item.username}</h3>
+            <h3>Email: {item.email} </h3>
+            <h3>Password: {item.password} </h3>
+          </div>
+
+          ))
+          
+          }
+       
+        </div>
+
+   
+      </h1>
+    </div>
+  )
+}
+
+export default App
